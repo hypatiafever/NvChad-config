81408745c5a1576ea92a6a53b986a30efc250bb1
Olivia <olireggiardo@gmail.com>
2025-05-22 14:24:49 -0300

    idk what this was

--- lua/custom/chadrc.lua
+++ lua/custom/chadrc.lua
@@ -1,7 +1,27 @@
 ---@type ChadrcConfig
 local M = {}
 
-M.ui = { theme = 'catppuccin' }
+M.ui = {
+  theme = 'catppuccin',
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+  },
+  -- akjsdbnajcbali
+  changed_themes = {
+    catppuccin = {
+      base_30 = {
+        grey_fg = "#797985"
+      },
+      base_16 = {
+        base03 = "#797985"
+      }
+    }
+  }
+}
 M.plugins = "custom.plugins"
 M.mappings = require("custom.mappings")
+
+
+
 return M
